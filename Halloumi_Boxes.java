b5de3245dee6a9d33284a06ab8920a2d45da6c3a
codebang45 <indoliajatin4@gmail.com>
2026-09-15 23:59:21 +0530

    next one

--- Halloumi_Boxes.java
+++ Halloumi_Boxes.java
@@ -1,6 +1,4 @@
-
 import java.util.*;
-
 class Halloumi_Boxes {
     public static void main(String[] args) {
 
---
+++ Line_Trip.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Line_Trip {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t > 0) {
+
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+
+            int arr[] = new int[n];
+
+            // Stations input
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            // 0 -> first station
+            int max = arr[0];
+
+            // Station -> station
+            for (int i = 0; i < arr.length - 1; i++) {
+                int distance = arr[i + 1] - arr[i];
+                max = Math.max(max, distance);
+            }
+
+            // Last station -> x -> last station
+            int lastDistance = 2 * (x - arr[n - 1]);
+            max = Math.max(max, lastDistance);
+
+            System.out.println(max);
+
+            t--;
+        }
+    }
+}
